6301279de2d99c22eec2eb81a5d4e5616e0aef9b
DebojitDas2003 <debojitdas0110@gmail.com>
2023-11-01 22:35:05 +0530

    initialization

---
+++ backend/controllers/productController.js
@@ -0,0 +1,93 @@
+const Product = require("../models/productModel");
+
+//create product -- Admin
+exports.createProduct = async (req, res, next) => {
+
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+
+//Get all product
+exports.getAllProducts = async (req, res) => {
+
+
+    const products = await Product.find();
+
+
+    res.status(200).json({
+        success: true,
+        products
+    })
+}
+
+//get single product
+
+exports.singleProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+//Update product
+
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+
+//delete Product
+
+exports.deleteProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    await Product.deleteOne({ _id: req.params.id });
+
+    res.status(200).json({
+        success: true,
+        message: "Product Deleted Successfully"
+    })
+}
+
+
